6d5c2414fa3cac8ea4cecf75cc8c6aabfa826e1f
ElevenVanMac <f35j10@163.com>
2015-04-28 23:45:55 +0800

    day1

---
+++ day1.py
@@ -0,0 +1,41 @@
+__author__ = 'ElevenVan'
+
+# #if
+#
+# x=int(input("Plz enter an integer:"))
+# if x < 0:
+#     x=0
+#     print("Negative changed to zero")
+# elif x==0:
+#     print("zero")
+# elif x==1:
+#     print("single")
+# else:
+#     print("more")
+
+# # for
+#
+# a=['cat','windows','defebestrate']
+# for x in a:
+#     print(x,len(x))
+
+
+# # range()
+#
+# for i in range(5):
+#     print(i,end=',')
+
+# a=['may', 'had', 'a', 'little', 'lamb']
+# for i in range(len(a)):
+#     print(i, a[i])
+
+
+
+# print(range(10))
+
+print(range(5))
+
+lists= list(range(5))
+print(lists)
+
+
