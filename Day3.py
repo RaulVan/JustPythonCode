b70098ab03b7ca2cd5ed0abc9cd5ce17e1127142
ElevenVan <f35j10@163.com>
2015-05-02 17:45:08 +0800

    day3...DW(慢速德语

--- Day3.py
+++ Day3.py
@@ -1,6 +1,6 @@
 __author__ = 'Eleven'
 #
-# # encoding:UTF-8
+# encoding:UTF-8
 # import urllib.request
 #
 # url = "http://www.baidu.com"
@@ -23,6 +23,7 @@ url_dw="http://de.hujiang.com/zt/dw"
 rh = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.116 Safari/537.36'}
 page=""
 
+
 def getHtmlPage(url):
     cookie = urllib.request.HTTPCookieProcessor(http.cookiejar.CookieJar())
 
@@ -30,31 +31,53 @@ def getHtmlPage(url):
     page = urllib.request.urlopen(req).read()
     page = page.decode('utf-8')
     return page
-
+urlDic=dict()
+urlKeys=list()
 
 def getListAtag(page):
     soup=BeautifulSoup(page)
     all_div=soup.find_all("div",class_="in_list_conent")
-    # print (all_div)
+    #print (all_div)
     # return page
     for div in all_div:
-        li_a=div.find_all('a')
-        # print(li_a)
-        print(li_a[0])
-        f_Url=li_a[0].get('href')
-        print(url_de_root+f_Url)
-        page1= getHtmlPage(url_de_root+f_Url)
+        li_as=div.find_all('li',class_="unit")
+        #print(li_as)
+        for a in li_as:
+            #print(a.find('a'))
+            a=a.find('a')
+            print( a.get_text()+(url_de_root+a.get('href')))
+            #print("%s  %s  "%(a.get_text(),url_de_root+a.get('href')))
+            #urlDic.update(a.get_text(),url_de_root+a.get('href'))
+            #?????????¡ì?????????
+            urlDic[a.get_text()]=url_de_root+a.get('href')
+            urlKeys.append(a.get_text())
+    return urlDic
+      
+        # page1= getHtmlPage(url_de_root+f_Url)
         # print(page1)
-# 编辑点评 div id="article_summary" class="article_summary"
+# ???¡§¡§?????¡§¡§?? div id="article_summary" class="article_summary"
 
 def getPageConnect(htmlStr):
      soup=BeautifulSoup(htmlStr)
 
 
 
-
 html=getHtmlPage(url_dw)
-getListAtag(html)
+dictcc= getListAtag(html)
 
-# print(page)
+print(dictcc.get(urlKeys[0]))
+
+#html=getHtmlPage(dictcc.items())
+#print(html)
+print("OK")
+
+
+
+#TODO：先保存到文本。。。。然后上传
+
+
+
+ 
+
+
 
